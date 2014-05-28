f17ae04f35947f19a5a70c6a80bdb330d5ae21e1
daniel_hk <daniel.p6800@gmail.com>
2014-05-28 21:01:58 +0800

    remove unuse cdma.mk
    cdma.mk not needed in cm-11.0

--- device.mk
+++ device.mk
@@ -25,7 +25,6 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 $(call inherit-product, build/target/product/telephony.mk)
-$(call inherit-product, vendor/cm/config/cdma.mk)
 
 PRODUCT_PACKAGES += \
 	libsecril-client
