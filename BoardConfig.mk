3af0d7f7248ab80c57a10a1a4fb1d8c4c0bbd054
danielhk <daniel.p6800@gmail.com>
2015-01-11 22:13:54 +0800

    Add radio flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,3 +26,4 @@ TARGET_KERNEL_CONFIG := cyanogenmod_i815_defconfig
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i815/bluetooth
 # @daniel added
 BOARD_USES_BH1721FVC := true
+TARGET_TOROPLUS_RADIO := true
