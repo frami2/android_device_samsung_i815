102f1ec5ff2f6a8b5b22a9cf6a9ce7acff7f8a90
daniel_hk <daniel.p6800@gmail.com>
2015-08-05 17:54:36 +0800

    Add support for Omni builds

---
+++ omni_i815.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 1280
+
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := i815
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SCH-I815
+PRODUCT_NAME := omni_i815
+PRODUCT_RELEASE_NAME := SCH-I815
+
+# Set product name and build fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCH-I815 BUILD_FINGERPRINT="samsung/SCH-I815/SCH-I815:4.3.1/JLS36I:user/release-keys" PRIVATE_BUILD_DESC="SCH-I815-user 4.3.1 JLS36I release-keys"
