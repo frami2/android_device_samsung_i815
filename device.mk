80cbb0c3d07e5687805e4e800c003fa0a7c5193f
daniel_hk <daniel.p6800@gmail.com>
2015-12-12 00:12:04 +0800

    Fix RIL for i815
    1. copy init.smdk4210-lte.rc to root
    2. Add Shim library
       Handle the two missing symbols

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +19,10 @@ $(call inherit-product-if-exists, vendor/samsung/i815/vendor.mk)
 # Set preferred size for assets
 PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
+# use init.smdk4210-lte.rc for i815
+PRODUCT_COPY_FILES += \
+	device/samsung/smdk4210-tab/rootdir/init.smdk4210-lte.rc:root/init.smdk4210.rc
+
 # GPS
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/etc/gps.conf:system/etc/gps.conf
@@ -30,5 +34,10 @@ PRODUCT_COPY_FILES += \
 	device/samsung/smdk4210-tab/configs/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
+# Samsung symbols
+PRODUCT_PACKAGES += \
+	libsamsung_symbols
+
+# extra packages
 PRODUCT_PACKAGES += \
 	libsecril-client
